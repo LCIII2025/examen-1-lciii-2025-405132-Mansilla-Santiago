aba8d9c377291f00b0f60c239f3ec6294a16a3b2
405132-Mansilla-Santiago <405132@tecnicatura.frc.utn.edu.ar>
2025-05-22 19:50:20 -0300

    feature: finish methods in Estacionamiento.java

--- src/main/java/org/example/parking/Estacionamiento.java
+++ src/main/java/org/example/parking/Estacionamiento.java
@@ -8,21 +8,46 @@ public class Estacionamiento {
     private final Map<String, Cliente> clientesRegistrados = new HashMap<>();   //DNI / Cliente
 
     public boolean ingresarVehiculo(String dni, String nombre, Vehiculo vehiculo) {
-        // TODO implementar la logica para registrar el ingreso de un nuevo vehiculo en el parking
+        // DONE implementar la logica para registrar el ingreso de un nuevo vehiculo en el parking
         // la capacidad maxima del estacionamiento es de 50 vehiculos, si supera esta cap[acidad retornar FALSE
         // validar que no exista otro vehiculo con la misma patente, es un caso de error, retornar FALSE
         // validar si existe el cliente registrado, agregar el nuevo vehiculo en la lista del cliente existente, caso contrario crear un nuevo registro
         // si el proceso es exitoso retornar TRUE
+        if (vehiculosEstacionados.size() >= capacidadMaxima) {
+            return false;
+        }
+        if (vehiculosEstacionados.containsKey(vehiculo.getPatente())) {
+            return false;
+        }
 
-        return false;
+        if (clientesRegistrados.containsKey(dni)) {
+            Cliente registerClient = clientesRegistrados.get(dni);
+            registerClient.agregarVehiculo(vehiculo);
+        } else {
+            Cliente client = new Cliente(dni, nombre);
+            client.agregarVehiculo(vehiculo);
+            clientesRegistrados.put(dni, client);
+        }
+        Ticket newTicket = new Ticket(clientesRegistrados.get(dni), vehiculo);
+        vehiculosEstacionados.put(vehiculo.getPatente(), newTicket);
+
+        return true;
     }
 
     public Ticket retirarVehiculo(String patente) throws Exception {
-        // TODO implementar la lógica para retirar un vehiculo del parking
+        // DONE implementar la lógica para retirar un vehiculo del parking
         // validar que exista la patente, caso contrario arrojar la exception "Vehiculo no encontrado"
         // calcular y retornar el ticket del vehiculoEstacionado (ver Ticket.marcarSalida())
+        if (!vehiculosEstacionados.containsKey(patente)) {
+            throw new Exception("Vehiculo no encontrado");
+        }
 
-        return null;
+        Ticket ticket = vehiculosEstacionados.get(patente);
+        ticket.marcarSalida();
+               
+        vehiculosEstacionados.remove(patente);
+
+        return ticket;
     }
 
     public List<Ticket> listarVehiculosEstacionados() {
